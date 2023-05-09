9b4f3d329b519158e0fd82b8521322bd1dab17b9
Christian Platt <christian.platt@pharmaline.de>
2023-05-09 23:27:57 +0200

    V10 RC

--- ext_emconf.php
+++ ext_emconf.php
@@ -20,10 +20,10 @@ $EM_CONF[$_EXTKEY] = [
     'uploadfolder' => 0,
     'createDirs' => '',
     'clearCacheOnLoad' => 0,
-    'version' => '1.0.6',
+    'version' => '1.10.4',
     'constraints' => [
             'depends' => [
-                'typo3' => '9.5.0-9.5.99',
+                'typo3' => '9.5.0-10.4.99',
             ],
             'conflicts' => [],
             'suggests' => [],
